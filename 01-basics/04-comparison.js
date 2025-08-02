3cada7cab77f8912d012d09d01b39ffa480d4dc7
Anand Dwivedi <139412006+mranand2004@users.noreply.github.com>
2025-08-02 10:33:05 +0000

    datatypes

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,19 @@
+// we need same data type to compare
+
+
+console.log("2">1);// true
+console.log("02">1);//true
+
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+
+// it compare only value
+//===  it compare data and value both
+console.log("2"===2);
