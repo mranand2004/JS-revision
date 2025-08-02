df4505e30b6bd77d3654841c4ef3e0d672de0445
Anand Dwivedi <139412006+mranand2004@users.noreply.github.com>
2025-08-02 07:26:50 +0000

    datatypes

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version.
+
+// alert(3+3)  we are using nodejs, not browser
+console.log(3
+    +3);  // code readability should be high
+onsole.log("Anand")
+
+
+console.log(typeof "Anand");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+//bigints
+//boolean- t/f
+//number
+// string--""
+//null-- standalone value
+// undefine=>
+//symbol--unique
