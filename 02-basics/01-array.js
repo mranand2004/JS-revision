d224a332bebab93b946a716e8fcc843cf9896eb7
Anand Dwivedi <139412006+mranand2004@users.noreply.github.com>
2025-08-05 12:28:32 +0000

    Arrays

---
+++ 02-basics/01-array.js
@@ -0,0 +1,33 @@
+// Array
+const myArr=[0,1,2,3,4,5]
+const myHeroes=["Anand", "Ayush"]
+
+
+const myArr2 =   Array[1,2,3,4]
+console.log(myArr[1]);
+
+//Array Methods
+
+
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+
+console.log(myArr)
+
+myArr.unshift(9);
+myArr.shift();
+
+console.log(myArr.includes(9));
+console.log(myArr.includes(3));
+
+// splice and alice
+
+console.log("A" , myArr)
+const myn1= myArr.slice(1,3);
+
+console.log(myn1)
+console.log("B", myArr);
+
+
+
